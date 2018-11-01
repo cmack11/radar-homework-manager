9566ed713e743a61eca7f8dd33db3926b414530e
Natasha <natashaefendy@gmail.com>
2018-10-31 20:31:55 -0500

    Use fake data in Redux

--- src/reducers/assignmentReducer.js
+++ src/reducers/assignmentReducer.js
@@ -2,26 +2,29 @@ import * as types from '../actions/action_types.js';
 import {axios} from 'axios';
 
 let initialState = {
-  name:"",
-  color:"",
-  assignments:[]
+  subjects : []
 }
 export default (state = initialState, action) => {
  switch (action.type) {
-  case types.INITIALIZE_USER:
+  case types.INITIALIZE_ASSIGNMENTS:
    return {
     ...state,
-    name: action.payload.name,
-    color : action.payload.color,
-    assignments: action.payload.assignments
+    subjects : action.payload.subjects
    }
    case types.UPDATE_ASSIGNMENT :
     return {
       ...state,
-      name: action.payload.name,
-      color : action.payload.color,
-      assignments: action.payload.assignments
+      subjects : action.payload.subjects
     }
+    case types.ADD_ASSIGNMENT :
+      let newSubjectList = state.subjects.map((el, index)=> {
+        if (el.id == action.payload.subject.id) el.assignment.append(action.payload.assignment);
+        return el;
+      })
+      return {
+        ...state,
+        subjects: newSubjectList
+      }
   default:
    return state
  }
