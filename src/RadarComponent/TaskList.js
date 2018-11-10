11464062b0950c3db94db00adc4ddd38cb3dcfa8
Ethan White <>
2018-11-10 14:12:16 -0600

    Adding the new TaskList component to display a list of tasks. Please look at the top of this file for information on what props to pass in.

---
+++ src/RadarComponent/TaskList.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+import {subjects1} from '../fakeData.js'
+
+/*
+TaskList props:
+    visible = true/false - defaults true
+    title = String (This title will be shown above the table)
+    width
+    height
+    assignments = array of assignments to display.
+        assignments should have a .name, .subject, .description, .type, and .dueDate (moment)
+*/
+
+class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+    let visible = true;
+    if (this.props.visible !== undefined && !this.props.visible)
+        visible = false;
+    this.state = {visible: visible};
+  }
+
+  render() {
+    if (!this.state.visible)
+        return(<null />);
+
+    let columns =
+    [
+        {
+            Header: 'Name',
+            accessor: 'name'
+        },
+        {
+            Header: 'Subject',
+            accessor: 'subject'
+        },
+        {
+            Header: 'Description',
+            accessor: 'description'
+        },
+        {
+            Header: 'Type',
+            accessor: 'type'
+        },
+        {
+            Header: 'Due Date',
+            accessor: 'dueDate',
+            Cell: props => <span className='dueDate'>{props.value.format("MM/DD/YYYY")}</span>
+        }
+    ];
+    const data = this.props.assignments !== undefined ? this.props.assignments : subjects1[0].assignments;
+
+    return (
+    <div style={{
+            width:this.props.width,
+            height:this.props.height,
+            padding:20
+        }}>
+        {this.props.title}
+        <ReactTable
+            data={data}
+            columns={columns} 
+            defaultPageSize={5}
+            pageSizeOptions = {[5, 10, 15]}
+        />
+    </div>  
+    );
+  }
+}
+
+export default TaskList;
