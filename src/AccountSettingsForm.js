23c4caf4fd9dce7f62d6b8c651bc66beef879a8d
Natasha <natashaefendy@gmail.com>
2018-12-12 15:23:41 -0600

    Fix input issue in account settings

--- src/AccountSettingsForm.js
+++ src/AccountSettingsForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import DatePicker from "react-datepicker";
 import {Button, Form} from 'semantic-ui-react';
+import './App.css';
 
 const mapDispatchToProps = dispatch => ({
  //TODO
@@ -25,7 +26,6 @@ export class AccountSettingsForm extends React.Component {
     super(props);
 
     this.state = this.getDefaultState();
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -34,91 +34,22 @@ export class AccountSettingsForm extends React.Component {
     return defaultState;
   }
 
-  handleChange(event) {
-    const target = event.target;
-    const name = target.name;
-    let value = target.value;
-
-    if(name === 'Name') {
-      if(value.length > 0)
-        this.setState({NameError:false});
-      else
-        this.setState({NameError:true});
-    }
-
-    this.setState({
-      [name]: value
-    });
-  }
-
   handleSubmit(event) {
-    if(!this.allValid()) return;
+
+    if (this.refs.newName.value == this.props.name)
+      return
 
     //call redux function to pass new name and password to database
-
-	/*
-    let assignment = this.props.assignment;
-    assignment.name = this.state.taskName;
-    assignment.description = this.state.taskDesc;
-    assignment.type = this.state.taskType;
-    assignment.dueDate = this.state.taskDueDate.format(DATE_FORMAT)
-    console.log(this.state)
-    let subject_id;
-    for(let i = 0; i < this.props.subjects.length && !subject_id; i++) {
-      let s = this.props.subjects[i];
-      for(let j = 0; j < s.assignments.length && !subject_id; j++) {
-        let a = s.assignments[j];
-        if(a.task_id === assignment.task_id)
-          subject_id = s.subject_id;
-      }
-    }
-
-    //console.log("New subject is " + JSON.stringify(d))
-    this.props.newAssignment(d);
-    this.setState(this.getDefaultState());
-    if(this.props.closeForm)
-    	this.props.closeForm();
-  	}else{
-  		this.props.addAssignment(
-      	{subject:this.state.subject, name:this.state.taskName, description:this.state.taskDesc, type:this.state.taskType, dueDate:this.state.taskDueDate},
-      	this.state.subject);
-      	this.setState(this.getDefaultState());
-      	if(this.props.closeForm)
-        		this.props.closeForm();
-  	}
-    let user_id = this.props.id;
-
-    this.props.editTask(assignment,subject_id,user_id);
-
-    this.setState(this.getDefaultState());
-    */
 }
-
-
-  allValid() {
-    if(this.state.Name === "" || this.state.Name.length > maxNameLength || this.state.Name.length < minNameLength) {
-      this.setState({NameError:true});
-      return false;
-    }
-    if(this.state.Password === "" || this.state.Password.length > maxPasswordLength || this.state.Password.length < minPasswordLength) {
-      this.setState({PasswordError:true});
-      return false;
-    }
-    return true
-  }
 
   render() {
 
     return (
-      <div style={{position: 'absolute', left: "15%", top: 0, width: '50%', height: '80%', margin: '10%', fontSize:16}} onClick={(e) => {e.stopPropagation()}}>
-
-      <Form >
-
+      <div style={{position: 'absolute', left: "15%", top: 0, width: '50%', height: '80%', margin: '10%', fontSize:16}} >
         <div className="account-info-container" >
          	<b>Account Information</b>
         </div>
-        <br />
-
+        <Form>
         <Form.Field className='form-fields'>
           <label className="label-text label-center"> Name:  </label>
           <label> {this.state.Name} </label>
@@ -137,22 +68,22 @@ export class AccountSettingsForm extends React.Component {
 
         <Form.Field className='form-fields'>
           <label className="label-text label-center">New Name:</label>
-          <input name="Name" type="text" value="" onChange={this.handleChange} />
+          <input ref="newName"/>
         </Form.Field>
 
         <Form.Field className='form-fields'>
           <label className="label-text label-center">New Password:</label>
-          <input name="Password" type="text" value="" onChange={this.handleChange} />
+          <input ref="newPass"/>
         </Form.Field>
 
         <Form.Field className='form-fields'>
           <label className="label-text label-center">Confirm New Password:</label>
-          <input name="Password" type="text" value="" onChange={this.handleChange} />
+          <input ref="confirmPass"/>
         </Form.Field>
 
         <Button primary type="button" value="Save Changes" onClick={this.handleSubmit}><b>Save Changes</b></Button>
-      </Form>
-      </div>
+        </Form>
+    </div>
     );
   }
 }
